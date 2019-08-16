b0a6a0ad1258f8bada254f675b57d4c152d5e5e1
Joe David <javid92@gmail.com>
2019-08-16 09:05:32 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,7 @@
 # Add your variables here
+first_number= 1.0
+second_number = 1.0
+sum = first_number + second_number
+difference = second_number - first_number
+product = first_number * second_number
+quotient = first_number / second_number
